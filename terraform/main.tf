1c9a8f40a5554a20f1283a5cbbf3d5d328447936
Luka Kidrič <luka.kidric@gmail.com>
2022-09-14 11:03:48 +0200

    added terraform workflow and updated main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,8 +39,8 @@ resource "google_project_service" "resourcemanager" {
   disable_on_destroy = false
 }
 
-# This is used so there is some time for the activation of the API's to propagate through
-# Google Cloud before actually calling them.
+# This is used so there is some time for the activation of the API's to propagate through Google Cloud before actually calling them.
+
 resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
   depends_on = [
