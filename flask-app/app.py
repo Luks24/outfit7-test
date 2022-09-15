db5ae2fc364b8d877472e234f4bdda743ac0eb30
Luka Kidrič <luka.kidric@gmail.com>
2022-09-15 16:52:11 +0200

    fixed flask app issue

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,11 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello_ekipa():
-
     return '<h1>Hello Outfit7</h2>'
+    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
